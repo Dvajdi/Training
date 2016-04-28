604a8531da2b52da74d1302d92445a76d462077b
twice <dvajdi@bk.ru>
2016-04-28 13:02:45 +0300

    SwipeListener от Климова (Загружалка для ScrollView)

--- app/src/main/java/ru/twice/forge/training/MainActivity.java
+++ app/src/main/java/ru/twice/forge/training/MainActivity.java
@@ -1,52 +1,58 @@
 package ru.twice.forge.training;
 
+
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+private SwipeRefreshLayout mSwipeRefreshLayout;
+private TextView mCatTextView;
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.content_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        mCatTextView = (TextView) findViewById(R.id.textViewCat);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        mSwipeRefreshLayout.setColorSchemeResources(
+                android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        // Альтернативный способ
+        //  mSwipeRefreshLayout.setColorSchemeColors(
+        //  Color.RED, Color.GREEN, Color.BLUE, Color.CYAN);
+
+        // Устаревший код
+        //  mSwipeRefreshLayout.setColorScheme(android.R.color.holo_blue_bright,
+        //          android.R.color.holo_green_light,
+        //          android.R.color.holo_orange_light,
+        //          android.R.color.holo_red_light);
         }
 
-        return super.onOptionsItemSelected(item);
-    }
-}
+@Override
+public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+
+@Override
+public void run() {
+        mSwipeRefreshLayout.setRefreshing(false);
+        Random rand = new Random();
+        mCatTextView.setText("Котика пора кормить. Его не кормили уже "
+        + (1 + rand.nextInt(10)) + " мин.");
+        }
+        }, 4000);
+        }
+        }
